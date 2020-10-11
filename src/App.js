9c3703a71fff9782a34331a72a4e87d5776ed9b1
Fernando Cesar <cmnando.silva@gmail.com>
2020-10-11 20:33:17 -0300

    Variáveis e funções JS

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import Title from "./HelloWorld"
+import React from "react";
 
 function App() {
+  const frase = "Hello World!";
+  var numero = 40;
+  function clicou(exibicao) {
+    alert(exibicao * 10);
+  }
   return (
-    <div className="App">
-      <Title></Title>
-      <Title></Title>
-      <Title></Title>
+    <div>
+      <h1>
+        {frase} {numero + 1}
+      </h1>
+      <button onClick={() => clicou(20)}>20</button>
+      <button onClick={() => clicou(100)}>100</button>
     </div>
   );
 }
-
-
 
 export default App;
